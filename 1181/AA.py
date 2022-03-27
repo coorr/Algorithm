7164c1fbaa018ade89f6d5a3718223cd28b8f066
kim jin seong <wlsdiqkdrk@naver.com>
2022-03-27 21:30:23 +0900

    03-27

---
+++ 1181/AA.py
@@ -0,0 +1,19 @@
+from dis import dis
+import sys
+sys.stdin=open("1181/input.txt","r")
+import math 
+
+n = int(sys.stdin.readline())
+lst = []
+
+for i in range(n):
+    lst.append(sys.stdin.readline().strip())
+set_lst = set(lst)
+lst = list(set_lst)
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
+                
+                
