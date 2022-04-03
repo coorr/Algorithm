97c376045d0d03fe0e5d95bc4b936b81f62a52d1
kim jin seong <wlsdiqkdrk@naver.com>
2022-04-03 23:17:09 +0900

    04-03

--- 4월/11726 2xn타일링/AA.py
+++ 4월/11726 2xn타일링/AA.py
@@ -5,8 +5,17 @@ sys.stdin=open("4월/11726 2xn타일링/input.txt","r")
 
 
 n=int(input())
+dy=[0] * (n+2)
+dy[1]=1
+dy[2]=2
+res=0
+for i in range(2,n+2):
+    dy[i]=2*dy[i-2]+dy[i-1]
+    if dy[i]>res:
+        res=dy[i]
+print(res%10007)
 
-
+    
         
 
 
