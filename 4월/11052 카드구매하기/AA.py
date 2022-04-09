975e02c841caf1d5c91cedbf0520825ab0b5e8c7
kim jin seong <wlsdiqkdrk@naver.com>
2022-04-09 13:10:03 +0900

    04-09

--- 4월/11052 카드구매하기/AA.py
+++ 4월/11052 카드구매하기/AA.py
@@ -2,15 +2,16 @@
 import sys
 sys.stdin=open("4월/11052 카드구매하기/input.txt","r")
 
-n = int(input())
-d = [0] * (n + 1)
-p = [0] + list(map(int, input().split()))
-d[1] = p[1]
-for i in range(2, n + 1):
-    for j in range(1, i + 1):
-        if d[i] < d[i - j] + p[j]:
-            d[i] = d[i - j] + p[j]
-print(d[n])
+N = int(input())
+p = [0] + list(map(int,input().split()))
+dp = [999999999 for i in range(1001)]
+dp[0] = 0
+
+
+for i in range(N+1):
+    for k in range(i+1):
+        dp[i] = min(dp[i], dp[i-k] + p[k])
+print(dp[N])
 
     
         
--- 4월/9095 1,2,3더하기/AA.py
+++ 4월/9095 1,2,3더하기/AA.py
@@ -3,8 +3,27 @@ import sys
 sys.stdin=open("4월/9095 1,2,3더하기/input.txt","r")
 
 
+
+n=int(input())  
+arr=list(sys.stdin.readline().split())
+arr.insert(0,0)
+dy=[0] * (n + 1)
+dy[1]=1
+print(arr)
+print(dy)
+res=0 
+for i in range(2,n+1):
+    Max=0
+    for j in range(i-1,0,-1):
+        if arr[i] > arr[j] and dy[j]>Max:
+            Max=dy[j]
+    dy[i]=Max+1
     
-        
+    if dy[i]>res:
+        res=dy[i]
+print(res)
+
+              
 
 
 
