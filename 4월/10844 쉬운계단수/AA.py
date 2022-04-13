56dc980e03efbbafe42db0eb0b6ae0895f2a319a
kim jin seong <wlsdiqkdrk@naver.com>
2022-04-14 02:04:56 +0900

    aiwacs img add

--- 4월/10844 쉬운계단수/AA.py
+++ 4월/10844 쉬운계단수/AA.py
@@ -1,25 +1,24 @@
 
+
 import sys
 sys.stdin=open("4월/10844 쉬운계단수/input.txt","r")
 
 n = int(input())
-dp = [[0 for i in range(10)] for j in range(101)]
-for i in range(1, 10):
-    dp[1][i] = 1
-for i in range(2, n + 1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i - 1][1]
-        elif j == 9:
-            dp[i][j] = dp[i - 1][8]
-        else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-print(sum(dp[n]) % 1000000000)
-
-    
-        
+List=[list(map(int,input().split())) for _ in range(n)]
+dy=[[0] * n for _ in range(n    )]
+dy[0][0]=List[0][0]
 
 
+for i in range(1,n):
+    dy[0][i] = dy[0][i-1] + List[0][i]
+    dy[i][0] = dy[i-1][0] + List[i][0]
+for i in range(1,n):
+    for j in range(1,n):
+        dy[i][j] = max(dy[i-1][j], dy[i][j-1])+List[i][j]
+
+for x in dy:
+    print(x)
+print(dy[n-1][n-1])
 
     
     
